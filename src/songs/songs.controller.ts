83275649ddb489bdb9b1ae2b5a23ee9037375947
TeodorTod <teodor.at.tod@gmail.com>
2025-03-16 19:48:45 +0200

    commit

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,33 +1,54 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongDTO } from './dto/create-song-dto';
 
 @Controller('songs')
 export class SongsController {
-    constructor(private songsService: SongsService) { }
+  constructor(private songsService: SongsService) {}
 
-    @Get()
-    findAll() {
-        return this.songsService.findAll();
+  @Get()
+  findAll() {
+    try {
+      return this.songsService.findAll();
+    } catch (error) {
+      throw new HttpException('server error', HttpStatus.BAD_REQUEST);
     }
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return `Find song with the id ${id}`;
-    }
+  @Get(':id')
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_FOUND }),
+    )
+    id: number,
+  ) {
+    return `Find song with the id ${typeof id}`;
+  }
 
-    @Post()
-    create(@Body() createSongDTO: CreateSongDTO) {
-        return this.songsService.create(createSongDTO);
-    }
+  @Post()
+  create(@Body() createSongDTO: CreateSongDTO) {
+    return this.songsService.create(createSongDTO);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string) {
-        return `Update song with the id ${id}`;
-    }
+  @Patch(':id')
+  update(@Param('id') id: string) {
+    return `Update song with the id ${id}`;
+  }
 
-    @Delete(':id')
-    delete(@Param('id') id: string) {
-        return `Delete song with the id ${id}`;
-    }
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `Delete song with the id ${id}`;
+  }
 }
