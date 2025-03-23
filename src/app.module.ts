cf16ee2eb8f41c42962ad43a3307064f883dc104
TeodorTod <teodor.at.tod@gmail.com>
2025-03-23 17:14:02 +0200

    new

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SongsModule } from './songs/songs.module';
-import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { UsersModule } from './users/users.module';
+import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [SongsModule],
+  imports: [UsersModule, TweetModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('songs');
-  }
-}
+export class AppModule {}
