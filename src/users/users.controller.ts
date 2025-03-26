cf9c2cf35d70aa05b645d9cc387c3199768c32f8
TeodorTod <teodor.at.tod@gmail.com>
2025-03-26 21:16:28 +0200

    user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
 
-    
+    @Get()
+    getUsers() {
+        return this.usersService.getAllUsers();
+    }
+
+    @Get(':id')
+    getUserById(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.getUserById(id);
+    }
+
+    @Post()
+    createUser(@Body() body: any) {
+        return this.usersService.createUser(body);
+    } 
 }
---
+++ src/users/users.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users: {
+    id: number;
+    name: string;
+    age: number;
+    gender: string;
+    isMarred: boolean;
+  }[] = [
+    {
+      name: 'John',
+      age: 20,
+      gender: 'male',
+      isMarred: false,
+      id: 1,
+    },
+    {
+      name: 'Jane',
+      age: 20,
+      gender: 'female',
+      isMarred: false,
+      id: 2,
+    },
+    {
+      name: 'Jack',
+      age: 20,
+      gender: 'male',
+      isMarred: false,
+      id: 3,
+    },
+  ];
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(user: { 
+    name: string;
+    age: number;
+    gender: string;
+    isMarred: boolean;
+    id: number;
+  }) {
+    this.users.push(user);
+  }
+}
