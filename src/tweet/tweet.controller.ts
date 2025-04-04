429b84320036dbbd0302b42bab54c51a8779c8db
TeodorTod <teodor.at.tod@gmail.com>
2025-04-04 22:18:45 +0300

    tweet service

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { TweetService } from './tweet.service';
 
 @Controller('tweet')
 export class TweetController {
     constructor(private readonly tweetService: TweetService) {}
+
+    @Get(':userId?')
+    public getTweets(@Param('userId', ParseIntPipe) userId: number) {
+        return this.tweetService.getTweets(userId);
+    }
 }
--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class TweetService {}
+export class TweetService {
+    constructor(private readonly usersService: UsersService) {}
+
+  tweets: { text: string; date: Date; userId: number }[] = [
+    { text: 'Hello1', date: new Date(), userId: 1 },
+    { text: 'Hello2', date: new Date(), userId: 2 },
+    { text: 'Hello3', date: new Date(), userId: 3 },
+  ];
+
+  getTweets(userId: number) {
+    const user = this.usersService.getUserById(userId);
+    const tweets = this.tweets.filter((tweet) => tweet.userId === userId);
+    const response = tweets.map(t => { return {text: t.text, date: t.date, user: user.name}});
+    return response;
+  }
+}
