1c106993949951aaae43c97fc1c21fceae9d560b
TeodorTod <teodor.at.tod@gmail.com>
2025-04-09 21:48:18 +0300

    typeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,32 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [
+    UsersModule,
+    TweetModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'admin',
+        password: '123456',
+        database: 'nestjs',
+        autoLoadEntities: true,
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
