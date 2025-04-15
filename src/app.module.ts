73781c5ab5720c7d036e07030b03b7a038b5a734
TeodorTod <teodor.at.tod@gmail.com>
2025-04-15 21:49:08 +0300

    many to many

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { ProfileModule } from './profile/profile.module';
+import { HashtagModule } from './hashtag/hashtag.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { ProfileModule } from './profile/profile.module';
       }),
     }),
     ProfileModule,
+    HashtagModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/hashtag/hashtag.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Hashtag } from './hashtag.entity';
+import { CreateHashtagDto } from './dto/create-hashtag.dto';
+
+@Injectable()
+export class HashtagService {
+  constructor(
+    @InjectRepository(Hashtag)
+    private readonly hashtagRepository: Repository<Hashtag>,
+  ) {}
+
+  public async createHashtag(createHashtagDto: CreateHashtagDto) {
+    const hashtag = await this.hashtagRepository.create(createHashtagDto);
+    return await this.hashtagRepository.save(hashtag);
+  }
+}
