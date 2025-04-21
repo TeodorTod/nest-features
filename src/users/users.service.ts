83fa1f54cef4c3dcb914ea864d40a33275fe1b5f
TeodorTod <teodor.at.tod@gmail.com>
2025-04-21 22:22:59 +0300

    exeptions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
 
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -15,24 +19,65 @@ export class UsersService {
     private profileRepository: Repository<Profile>,
   ) {}
 
-  getAllUsers() {
-    return this.userRepository.find({
-      relations: ['profile'],
-    });
+  public async getAllUsers() {
+    try {
+      return await this.userRepository.find({
+        relations: ['profile'],
+      });
+    } catch (error) {
+      if (error.code === 'ECONNREFUSED') {
+        throw new RequestTimeoutException(
+          'An error occurred while fetching users',
+          {
+            description: 'Could not connect to the database',
+          },
+        );
+      }
+    }
   }
 
   public async findUserById(id: number) {
-    return await this.userRepository.findOneBy({id});
+    return await this.userRepository.findOneBy({ id });
   }
 
   public async createUser(userDto: CreateUserDto) {
-    userDto.profile = userDto.profile ?? {};
-    const user = this.userRepository.create(userDto);
-    return this.userRepository.save(user);
+    try {
+      userDto.profile = userDto.profile ?? {};
+
+      const existingUser = await this.userRepository.findOne({
+        where: [{ email: userDto.email }, { username: userDto.username }],
+      });
+
+      if (existingUser) {
+        throw new BadRequestException(
+          'User with this email/username already exists',
+        );
+      }
+
+      const user = this.userRepository.create(userDto);
+      return await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === 'ECONNREFUSED') {
+        throw new RequestTimeoutException(
+          'An error occurred while fetching users',
+          {
+            description: 'Could not connect to the database',
+          },
+        );
+      }
+
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+
+      throw new BadRequestException(
+        error?.detail || 'Unexpected error occurred',
+      );
+    }
   }
 
   public async deleteUser(id: number) {
     await this.userRepository.delete(id);
-    return {deleted: true};
+    return { deleted: true };
   }
 }
