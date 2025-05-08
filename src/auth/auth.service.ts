238c18b0bcbcd59b488a42c0d7b5ce53ca784856
TeodorTod <teodor.at.tod@gmail.com>
2025-05-08 21:38:48 +0300

    compare passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { UsersService } from 'src/users/users.service';
 import authConfig from './config/auth.config';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { HashingProvider } from './provider/hashing.provider';
 
 @Injectable()
 export class AuthService {
@@ -12,16 +13,28 @@ export class AuthService {
     private readonly usersService: UsersService,
     @Inject(authConfig.KEY)
     private readonly authConfiguration: ConfigType<typeof authConfig>,
+    private readonly hashingProvider: HashingProvider,
   ) {}
 
   isAuthenticated: boolean = false;
 
   public async login(loginDto: LoginDto) {
     const user = await this.usersService.findUserByUsername(loginDto.username);
-    return user;
+    let isEqual: boolean = false;
+    isEqual = await this.hashingProvider.comparePassword(loginDto.password, user.password);
+    if (!isEqual) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return {
+      data: user,
+      success: true,
+      message: 'Login successful',
+    };
   }
 
   public async signup(createUserDto: CreateUserDto) {
     return await this.usersService.createUser(createUserDto);
   }
+
+  
 }
