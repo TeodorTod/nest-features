07c5187c9dde76b789e003bd8c044df97aa7bad3
TeodorTod <teodor.at.tod@gmail.com>
2025-05-11 22:28:49 +0300

    auth guard

--- src/auth/guards/authorize.guard.ts
+++ src/auth/guards/authorize.guard.ts
@@ -1,9 +1,21 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Request } from "express";
+import { JwtService } from "@nestjs/jwt";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class AuthorizeGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+         const request: Request = context.switchToHttp().getRequest();
+         const token = request.headers.authorization?.split(' ')[1];
+        //  if (!token) {
+        //     throw new UnauthorizedException('Token not provided');
+        //  }
+        //  const jwtService = new JwtService();
+        //  const decodedToken = jwtService.verify(token);
+        //  if (!decodedToken) {
+        //     throw new UnauthorizedException('Invalid token');
+        //  }
         return true;
     }
 }
